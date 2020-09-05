8fe0b312286ff3586735b8107aef35e4a293e6af
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2020-09-05 16:39:23 -0400

    Initial commit

---
+++ Assets/Connect Four/scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void load1player()
+    {
+        SceneManager.LoadScene("1vaigame");
+    }
+
+    public void load2player()
+    {
+        SceneManager.LoadScene("1v1game");
+    }
+}
